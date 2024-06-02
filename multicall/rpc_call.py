bf926c98c94f0abab3de8933f4dee4776927e4c2
Parker Burchett <parkerburchett@gmail.com>
2024-06-02 19:38:38 -0400

    now sync

--- multicall/rpc_call.py
+++ multicall/rpc_call.py
@@ -1,10 +1,11 @@
 import aiohttp
 import asyncio
+import time
 
 import json
 from web3.providers import HTTPProvider
 from aiolimiter import AsyncLimiter
-
+import requests
 
 RETRY_COUNT = 3  # Number of retries for the HTTP request
 
@@ -41,13 +42,21 @@ async def async_rpc_eth_call(w3: HTTPProvider, rpc_args, session: aiohttp.Client
                     response.raise_for_status()
 
 
-def sync_rpc_eth_call(w3: HTTPProvider, rpc_args):
-    """Make a single rpc ETH call. Useful for testing"""
-    return asyncio.run(_single_client_session_rpc_eth_call(w3, rpc_args))
+def sync_rpc_eth_call(w3, rpc_args):
+    endpoint_uri = w3.provider.endpoint_uri  # Assuming this is accessible like this
+    headers = {"content-type": "application/json"}
+    data = json.dumps({"params": rpc_args, "method": "eth_call", "id": 1, "jsonrpc": "2.0"})
 
-
-async def _single_client_session_rpc_eth_call(w3: HTTPProvider, rpc_args):
-    rate_limiter = AsyncLimiter(max_rate=1, time_period=1)  # 1 call/second
-
-    async with aiohttp.ClientSession() as session:
-        return await async_rpc_eth_call(w3, rpc_args, session, rate_limiter)
+    for attempt in range(RETRY_COUNT):
+        response = requests.post(endpoint_uri, headers=headers, data=data, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+            return bytes.fromhex(data["result"][2:])
+        elif response.status_code == 413:
+            print("the payload is too large, you need to break up the calls")
+            raise Exception("Payload too large")
+        elif response.status_code == 429 and attempt < RETRY_COUNT - 1:
+            print(f"429 error, waiting to retry, attempt {attempt}")
+            time.sleep(2**attempt)  # Exponential backoff
+        else:
+            response.raise_for_status()
